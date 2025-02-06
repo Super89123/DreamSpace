4cdf2b71a6fa1c5e1ee3ba1e7155091a83dab1cb
Ilya <tedswa@mail.ru>
2025-02-06 20:11:23 +0400

    iNITIAL COMMIT

---
+++ src/main/java/dev/relaxertime/dreamSpace/DreamSpace.java
@@ -0,0 +1,17 @@
+package dev.relaxertime.dreamSpace;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DreamSpace extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
